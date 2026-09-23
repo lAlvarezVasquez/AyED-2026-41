bc4264b5fa771c3a1e3b40616c6d9a2dd8fd57bd
ALVAREZ LEANDRO <alvarezleandro203@gmail.com>
2026-09-23 10:44:39 -0300

    Agrego tp

---
+++ etapa3/5_CentroPokeRemedio/5_CentroPokeRemedio/5_CentroPokeRemedio/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_CentroPokeRemedio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion = 2;
+            int lista = 0;
+
+            int posicion = 0; 
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("C E N T R O  P O K É - R E M E D I O");
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("Elija una opcióbn: ");
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("1- Registrar un nuevo pokemón");
+            Console.WriteLine("2- Mostrar la vida de todos los pokemones");
+            Console.WriteLine("3- Curar un pokemón");
+            Console.WriteLine("4- Dañar un pokemon");
+            Console.WriteLine("5- Curar a todos los pokemones");
+            Console.WriteLine("6- Mostrar pokemones debilitados");
+            Console.WriteLine("7- Mostrar el pokemon con mayor vida");
+            Console.WriteLine("8- Mostrar el pokemon con menor vida");
+            Console.WriteLine("9- Calcular promedio de vida del equipo");
+            Console.WriteLine("10- Ordenar pokemones por vida de menor a mayor");
+            Console.WriteLine("11- Ordenar pokemones por vida de mayor a menor");
+            Console.WriteLine("12- Simular ataque enemigo a todo el equipo");
+            Console.WriteLine("13- Salir");
+            opcion = int.Parse(Console.ReadLine());
+            int[] vidaDePokemon = new int[6];
+            
+            switch (opcion)
+            {  
+                case 1 :
+                    if (lista < 6)
+                    {
+                        Console.Write("Ingresa la vida de tu pokemon : ");
+                        Console.ReadLine();
+
+                    }           
+                    
+                    break;
+            }
+
+
+        }
+    }
+}
---
+++ etapa3/6_MiPrimerMatriz/6_MIPrimerMatriz/6_MIPrimerMatriz/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_MIPrimerMatriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int filas;
+            int columnas;
+            Console.Write("Pon la cantidad de filas : ");
+            filas = int.Parse(Console.ReadLine());
+            Console.Write("Pon la cantidad de  columnas : ");
+            columnas = int.Parse(Console.ReadLine());
+            int[,] matriz = new int[filas, columnas];
+            for (int i = 0; i < filas; i++)
+            {
+                for (int j = 0; j < columnas; j++)
+                {
+                    Console.Write(matriz[i, j]);
+                }
+                Console.WriteLine(" ");
+
+
+
+
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
